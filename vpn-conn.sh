9d9493550865b14267384851de7b0e04bc8f9a78
Nazareno Gerbaldo <nazarenogerbaldo@gmail.com>
2021-05-20 00:48:12 -0300

    hot fix

--- vpn-conn.sh
+++ vpn-conn.sh
@@ -5,7 +5,7 @@ ST="www.stackoverflow.com"
 
 # VPN name and avg latency to a temporal file
 RES="/tmp/res.txt"
-VPNS"/tmp/vpns.txt"
+VPNS="/tmp/vpns.txt"
 
 # Ping to get avg latency
 ping -c 4 www.stackoverflow.com | tail -1| awk '{print $4}' | cut -d '/' -f 2
@@ -15,10 +15,10 @@ nmcli con | grep "vpn" | awk '{print $1}' > $VPNS
 
 while read vpn;
 do
-    echo -n "${vpn}" >> $RES
+    echo -n "${vpn} " >> $RES
     nmcli con up id ${vpn}
-    ping -c 1 "$ST" | tail -1 | awk '{print $4}' | cut -d '/' -f 2 >> $RES
-    nmcli con down id $̣{vpn}
+    ping -c 2 "$ST" | tail -1 | awk '{print $4}' | cut -d '/' -f 2 >> $RES
+    nmcli con down id ${vpn}
 done < $VPNS
 
 # Id of the ll-vpn
