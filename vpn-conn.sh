abdd90cf75909b6466e06942c615d89fbdb0038b
Nazareno Gerbaldo <nazarenogerbaldo@gmail.com>
2021-05-20 00:18:50 -0300

    All cmds ready

--- vpn-conn.sh
+++ vpn-conn.sh
@@ -11,3 +11,9 @@ ping -c 4 www.stackoverflow.com | tail -1| awk '{print $4}' | cut -d '/' -f 2
 
 # List of VPNs
 nmcli con | grep "vpn" | awk '{print $1}' > $RES
+
+# Sort by lowest latency
+sort -nk 2 $RES
+
+# Id of the ll-vpn
+$(sort -nk 2 $RES | head -1 | awk '{print $2}')
