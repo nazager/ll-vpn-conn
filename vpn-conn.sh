953355758b7e03c120105dc117df86276a0fe84d
Nazareno Gerbaldo <nazarenogerbaldo@gmail.com>
2021-05-20 00:14:14 -0300

    List of VPNs to a temp file

--- vpn-conn.sh
+++ vpn-conn.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-#Ping to get avg latency
+# Ping to stackoverflow to measure latency
+ST="www.stackoverflow.com"
+
+# VPN name and avg latency to a temporal file
+RES="/tmp/res.txt"
+
+# Ping to get avg latency
 ping -c 4 www.stackoverflow.com | tail -1| awk '{print $4}' | cut -d '/' -f 2
 
-#List of VPNs
-nmcli con | grep "vpn" | awk '{print $1}'
+# List of VPNs
+nmcli con | grep "vpn" | awk '{print $1}' > $RES
