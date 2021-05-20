4701806e1ad2dcdd1eaac87c79e1d13f22d19c5b
Nazareno Gerbaldo <nazarenogerbaldo@gmail.com>
2021-05-20 00:26:00 -0300

    Every avg latency con a temp file

--- vpn-conn.sh
+++ vpn-conn.sh
@@ -5,15 +5,18 @@ ST="www.stackoverflow.com"
 
 # VPN name and avg latency to a temporal file
 RES="/tmp/res.txt"
+VPNS"/tmp/vpns.txt"
 
 # Ping to get avg latency
 ping -c 4 www.stackoverflow.com | tail -1| awk '{print $4}' | cut -d '/' -f 2
 
 # List of VPNs
-nmcli con | grep "vpn" | awk '{print $1}' > $RES
+nmcli con | grep "vpn" | awk '{print $1}' > $VPNS
 
-# Sort by lowest latency
-sort -nk 2 $RES
-
-# Id of the ll-vpn
-$(sort -nk 2 $RES | head -1 | awk '{print $2}')
+while read vpn;
+do
+    echo -n "${vpn}" >> $RES
+    nmcli con up id ${vpn}
+    ping -c 1 "$ST" | tail -1 | awk '{print $4}' | cut -d '/' -f 2 >> $RES
+    nmcli con down id $̣{vpn}
+done < $VPNS
